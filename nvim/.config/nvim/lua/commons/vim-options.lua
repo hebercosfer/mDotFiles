1d92e3019faaf6838324e68a36f629a6169aa80c
Heber Costa Ferreira <costah@external.miltenyi.com>
2025-04-29 12:50:41 +0200

    NVIM: New key mappings

--- nvim/.config/nvim/lua/commons/vim-options.lua
+++ nvim/.config/nvim/lua/commons/vim-options.lua
@@ -14,3 +14,16 @@ vim.keymap.set("n", "<c-h>", ":wincmd h<CR>")
 vim.keymap.set("n", "<c-l>", ":wincmd l<CR>")
 vim.keymap.set("n", "<c-s>", ":w<CR>")
 vim.wo.number = true
+
+vim.keymap.set("n", "<leader>qq", vim.cmd.Ex)
+vim.keymap.set("n", "<C-d>", "<C-d>zz")
+vim.keymap.set("n", "<C-u>", "<C-u>zz")
+
+-- next greatest remap ever : asbjornHaland
+vim.keymap.set({ "n", "v" }, "<leader>y", [["+y]])
+vim.keymap.set("n", "<leader>Y", [["+Y]])
+
+vim.keymap.set("n", "<leader>f", vim.lsp.buf.format)
+
+vim.keymap.set("n", "<leader>bn", "<cmd>bnext<CR>")
+vim.keymap.set("n", "<leader>bp", "<cmd>bprev<CR>")
