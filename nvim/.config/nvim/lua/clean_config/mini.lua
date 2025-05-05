957fc06a6c6155e40a14b6fb18e1a89f69026856
Heber Costa Ferreira <costah@external.miltenyi.com>
2025-05-05 15:32:44 +0200

    NVIM: Mini additions and NeoTree fix

--- nvim/.config/nvim/lua/clean_config/mini.lua
+++ nvim/.config/nvim/lua/clean_config/mini.lua
@@ -1,17 +1,23 @@
 -- lua/custom/plugins/mini.lua
 return {
-	{
-		"echasnovski/mini.nvim",
-		version = "*",
-		enabled = true,
-		config = function()
-			local statusline = require("mini.statusline")
-			statusline.setup({ use_icons = true })
+  {
+    "echasnovski/mini.nvim",
+    version = "*",
+    enabled = true,
+    config = function()
+      local statusline = require("mini.statusline")
+      statusline.setup({ use_icons = true })
 
-			require("mini.surround").setup()
-			require("mini.pairs").setup()
-			require("mini.git").setup()
-			require("mini.diff").setup()
-		end,
-	},
+      require("mini.icons").setup()
+      require("mini.ai").setup()
+      require("mini.surround").setup()
+      require("mini.pairs").setup()
+      -- require("mini.operators").setup()
+      require("mini.git").setup()
+      require("mini.diff").setup()
+      -- require("mini.animate").setup()
+      require("mini.indentscope").setup()
+      require("mini.notify").setup()
+    end,
+  },
 }
